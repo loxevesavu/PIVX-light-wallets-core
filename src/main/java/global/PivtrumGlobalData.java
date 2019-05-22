3ba6214bb950f58bcfecb231b87285fd93b82cd5
loxevesavu <48093064+loxevesavu@users.noreply.github.com>
2019-05-22 10:38:28 +0530

    Update PivtrumGlobalData.java

--- src/main/java/global/PivtrumGlobalData.java
+++ src/main/java/global/PivtrumGlobalData.java
@@ -13,13 +13,13 @@ public class PivtrumGlobalData {
 
     public static final String FURSZY_TESTNET_SERVER = "185.101.98.175";
 
-    public static final String[] TRUSTED_NODES = new String[]{"node.pivxwiki.org", "panther.pivxwiki.org", "pivx.warrows.fr"};
+    public static final String[] TRUSTED_NODES = new String[]{"iquidus.rainbit.me", "bulwark.rainbit.me", "rnbit1.rainbit.me"};
 
     public static final List<PivtrumPeerData> listTrustedHosts(){
         List<PivtrumPeerData> list = new ArrayList<>();
         list.add(new PivtrumPeerData(FURSZY_TESTNET_SERVER,8443,55552));
         for (String trustedNode : TRUSTED_NODES) {
-            list.add(new PivtrumPeerData(trustedNode,51472,55552));
+            list.add(new PivtrumPeerData(trustedNode,29116,55552));
         }
         return list;
     }
